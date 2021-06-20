9f99b12cdd216c2788e59d2126c6eefb82c67de8
shinta0806 <shinta.0806@gmail.com>
2021-06-20 11:02:21 +0900

    Ver 2.91 時点

--- ViewModels/MiscWindowViewModels/FindKeywordWindowViewModel.cs
+++ ViewModels/MiscWindowViewModels/FindKeywordWindowViewModel.cs
@@ -10,18 +10,14 @@
 
 using Livet;
 using Livet.Commands;
-using Livet.EventListeners;
 using Livet.Messaging;
-using Livet.Messaging.IO;
 using Livet.Messaging.Windows;
+
 using Shinta;
+
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using YukaLister.Models;
+
 using YukaLister.Models.SharedMisc;
 using YukaLister.Models.YukaListerModels;
 
@@ -157,6 +153,7 @@ namespace YukaLister.ViewModels.MiscWindowViewModels
 			{
 				Direction = parameter == YlConstants.FIND_DIRECTION_BACKWARD ? -1 : 1;
 				_viewTFoundsWindowViewModel.Messenger.Raise(new InteractionMessage(YlConstants.MESSAGE_KEY_FIND_KEYWORD));
+				IsActive = true;
 			}
 			catch (Exception excep)
 			{
