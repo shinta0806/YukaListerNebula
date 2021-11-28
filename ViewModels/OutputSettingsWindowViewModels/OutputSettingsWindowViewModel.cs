90fdc187357ec5a5266401e8b4ea4c4f4494b027
shinta0806 <shinta.0806@gmail.com>
2021-11-28 13:27:59 +0900

    Ver 4.71 時点

--- ViewModels/OutputSettingsWindowViewModels/OutputSettingsWindowViewModel.cs
+++ ViewModels/OutputSettingsWindowViewModels/OutputSettingsWindowViewModel.cs
@@ -153,38 +153,8 @@ namespace YukaLister.ViewModels.OutputSettingsWindowViewModels
 		// 出力されない項目
 		public ObservableCollection<String> RemovedOutputItems { get; set; } = new();
 
-		// 選択されている出力されない項目
-		private String? _selectedRemovedOutputItem;
-		public String? SelectedRemovedOutputItem
-		{
-			get => _selectedRemovedOutputItem;
-			set
-			{
-				if (RaisePropertyChangedIfSet(ref _selectedRemovedOutputItem, value))
-				{
-					ButtonAddOutputItemClickedCommand.RaiseCanExecuteChanged();
-				}
-			}
-		}
-
 		// 出力される項目
 		public ObservableCollection<String> AddedOutputItems { get; set; } = new();
-
-		// 選択されている出力される項目
-		private String? _selectedAddedOutputItem;
-		public String? SelectedAddedOutputItem
-		{
-			get => _selectedAddedOutputItem;
-			set
-			{
-				if (RaisePropertyChangedIfSet(ref _selectedAddedOutputItem, value))
-				{
-					ButtonRemoveOutputItemClickedCommand.RaiseCanExecuteChanged();
-					ButtonUpOutputItemClickedCommand.RaiseCanExecuteChanged();
-					ButtonDownOutputItemClickedCommand.RaiseCanExecuteChanged();
-				}
-			}
-		}
 
 		#endregion
 
@@ -198,190 +168,6 @@ namespace YukaLister.ViewModels.OutputSettingsWindowViewModels
 		public static ListenerCommand<String>? HelpClickedCommand
 		{
 			get => YukaListerModel.Instance.EnvModel.HelpClickedCommand;
-		}
-		#endregion
-
-		#region 出力項目追加ボタンの制御
-		private ViewModelCommand? _buttonAddOutputItemClickedCommand;
-
-		public ViewModelCommand ButtonAddOutputItemClickedCommand
-		{
-			get
-			{
-				if (_buttonAddOutputItemClickedCommand == null)
-				{
-					_buttonAddOutputItemClickedCommand = new ViewModelCommand(ButtonAddOutputItemClicked, CanButtonAddOutputItemClicked);
-				}
-				return _buttonAddOutputItemClickedCommand;
-			}
-		}
-
-		public Boolean CanButtonAddOutputItemClicked()
-		{
-			return SelectedRemovedOutputItem != null;
-		}
-
-		public void ButtonAddOutputItemClicked()
-		{
-			try
-			{
-				if (SelectedRemovedOutputItem == null)
-				{
-					return;
-				}
-
-				AddedOutputItems.Add(SelectedRemovedOutputItem);
-				SelectedAddedOutputItem = SelectedRemovedOutputItem;
-				RemovedOutputItems.Remove(SelectedRemovedOutputItem);
-			}
-			catch (Exception excep)
-			{
-				YukaListerModel.Instance.EnvModel.LogWriter.ShowLogMessage(TraceEventType.Error, "出力項目追加ボタンクリック時エラー：\n" + excep.Message);
-				YukaListerModel.Instance.EnvModel.LogWriter.ShowLogMessage(Common.TRACE_EVENT_TYPE_STATUS, "　スタックトレース：\n" + excep.StackTrace);
-			}
-		}
-		#endregion
-
-		#region 出力項目削除ボタンの制御
-		private ViewModelCommand? _buttonRemoveOutputItemClickedCommand;
-
-		public ViewModelCommand ButtonRemoveOutputItemClickedCommand
-		{
-			get
-			{
-				if (_buttonRemoveOutputItemClickedCommand == null)
-				{
-					_buttonRemoveOutputItemClickedCommand = new ViewModelCommand(ButtonRemoveOutputItemClicked, CanButtonRemoveOutputItemClicked);
-				}
-				return _buttonRemoveOutputItemClickedCommand;
-			}
-		}
-
-		public Boolean CanButtonRemoveOutputItemClicked()
-		{
-			return SelectedAddedOutputItem != null;
-		}
-
-		public void ButtonRemoveOutputItemClicked()
-		{
-			try
-			{
-				if (SelectedAddedOutputItem == null)
-				{
-					return;
-				}
-
-				RemovedOutputItems.Add(SelectedAddedOutputItem);
-				SelectedRemovedOutputItem = SelectedAddedOutputItem;
-				AddedOutputItems.Remove(SelectedAddedOutputItem);
-			}
-			catch (Exception excep)
-			{
-				YukaListerModel.Instance.EnvModel.LogWriter.ShowLogMessage(TraceEventType.Error, "出力項目削除ボタンクリック時エラー：\n" + excep.Message);
-				YukaListerModel.Instance.EnvModel.LogWriter.ShowLogMessage(Common.TRACE_EVENT_TYPE_STATUS, "　スタックトレース：\n" + excep.StackTrace);
-			}
-		}
-		#endregion
-
-		#region 出力項目上へボタンの制御
-		private ViewModelCommand? _buttonUpOutputItemClickedCommand;
-
-		public ViewModelCommand ButtonUpOutputItemClickedCommand
-		{
-			get
-			{
-				if (_buttonUpOutputItemClickedCommand == null)
-				{
-					_buttonUpOutputItemClickedCommand = new ViewModelCommand(ButtonUpOutputItemClicked, CanButtonUpOutputItemClicked);
-				}
-				return _buttonUpOutputItemClickedCommand;
-			}
-		}
-
-		public Boolean CanButtonUpOutputItemClicked()
-		{
-			if (String.IsNullOrEmpty(SelectedAddedOutputItem))
-			{
-				return false;
-			}
-			Int32 index = AddedOutputItems.IndexOf(SelectedAddedOutputItem);
-			return index >= 1;
-		}
-
-		public void ButtonUpOutputItemClicked()
-		{
-			try
-			{
-				if (String.IsNullOrEmpty(SelectedAddedOutputItem))
-				{
-					return;
-				}
-				Int32 selectedIndex = AddedOutputItems.IndexOf(SelectedAddedOutputItem!);
-				if (selectedIndex < 1)
-				{
-					return;
-				}
-				String aItem = SelectedAddedOutputItem!;
-				AddedOutputItems.Remove(aItem);
-				AddedOutputItems.Insert(selectedIndex - 1, aItem);
-				SelectedAddedOutputItem = aItem;
-			}
-			catch (Exception excep)
-			{
-				YukaListerModel.Instance.EnvModel.LogWriter.ShowLogMessage(TraceEventType.Error, "出力項目上へボタンクリック時エラー：\n" + excep.Message);
-				YukaListerModel.Instance.EnvModel.LogWriter.ShowLogMessage(Common.TRACE_EVENT_TYPE_STATUS, "　スタックトレース：\n" + excep.StackTrace);
-			}
-		}
-		#endregion
-
-		#region 出力項目下へボタンの制御
-		private ViewModelCommand? _buttonDownOutputItemClickedCommand;
-
-		public ViewModelCommand ButtonDownOutputItemClickedCommand
-		{
-			get
-			{
-				if (_buttonDownOutputItemClickedCommand == null)
-				{
-					_buttonDownOutputItemClickedCommand = new ViewModelCommand(ButtonDownOutputItemClicked, CanButtonDownOutputItemClicked);
-				}
-				return _buttonDownOutputItemClickedCommand;
-			}
-		}
-
-		public Boolean CanButtonDownOutputItemClicked()
-		{
-			if (String.IsNullOrEmpty(SelectedAddedOutputItem))
-			{
-				return false;
-			}
-			Int32 index = AddedOutputItems.IndexOf(SelectedAddedOutputItem);
-			return 0 <= index && index < AddedOutputItems.Count - 1;
-		}
-
-		public void ButtonDownOutputItemClicked()
-		{
-			try
-			{
-				if (String.IsNullOrEmpty(SelectedAddedOutputItem))
-				{
-					return;
-				}
-				Int32 selectedIndex = AddedOutputItems.IndexOf(SelectedAddedOutputItem);
-				if (selectedIndex < 0 || selectedIndex >= AddedOutputItems.Count - 1)
-				{
-					return;
-				}
-				String item = SelectedAddedOutputItem!;
-				AddedOutputItems.Remove(item);
-				AddedOutputItems.Insert(selectedIndex + 1, item);
-				SelectedAddedOutputItem = item;
-			}
-			catch (Exception excep)
-			{
-				YukaListerModel.Instance.EnvModel.LogWriter.ShowLogMessage(TraceEventType.Error, "出力項目下へボタンクリック時エラー：\n" + excep.Message);
-				YukaListerModel.Instance.EnvModel.LogWriter.ShowLogMessage(Common.TRACE_EVENT_TYPE_STATUS, "　スタックトレース：\n" + excep.StackTrace);
-			}
 		}
 		#endregion
 
@@ -562,7 +348,7 @@ namespace YukaLister.ViewModels.OutputSettingsWindowViewModels
 			_outputWriter.OutputSettings.OutputAllItems = OutputAllItems;
 
 			// 出力項目のリスト
-			_outputWriter.OutputSettings.SelectedOutputItems?.Clear();
+			_outputWriter.OutputSettings.SelectedOutputItems.Clear();
 			for (Int32 i = 0; i < AddedOutputItems.Count; i++)
 			{
 				Int32 item = Array.IndexOf(YlConstants.OUTPUT_ITEM_NAMES, (String)AddedOutputItems[i]);
@@ -570,7 +356,7 @@ namespace YukaLister.ViewModels.OutputSettingsWindowViewModels
 				{
 					continue;
 				}
-				_outputWriter.OutputSettings.SelectedOutputItems?.Add((OutputItems)item);
+				_outputWriter.OutputSettings.SelectedOutputItems.Add((OutputItems)item);
 			}
 		}
 
